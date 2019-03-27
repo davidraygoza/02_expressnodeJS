25beef9fa0826111c200a159d69755a0e55b3e3c
David Raygoza <jorgeramos@PG3.local>
2019-03-27 15:49:28 -0600

    Using json on response

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-	res.send("hello world...");
+	res.send("Hello world...");
 });
 app.get('/about',(req, res) => {
 	res.send('About GET');
 });
 app.post('/about',(req, res) => {
 	res.send('About POST');
+});
+app.get('/data',(req, res) => {
+	res.send('Response GET');
+});
+app.post('/data',(req, res) => {
+	res.json({	username:'David',
+				lastname: 'Raygoza'
+		});
 });
 app.get('/contact',(req, res) => {
 	res.send('form contact');
